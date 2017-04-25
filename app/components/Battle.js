451a2fa1cc9903d6bd7caed64fee5b45b668f3e0
Tunde <tundaey@gmail.com>
2017-04-25 23:54:05 +0100

    created and implemented the results component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -2,31 +2,7 @@ var React = require('react');
 var PropTypes = require('prop-types');
 var Link = require('react-router-dom').Link
 
-function PlayerPreview(props){
-	return (
-			<div>
-				<div className="column">
-					<img 
-						className="avatar"
-						src={props.avatar}
-						alt={'Avatar for  ' + props.username} />
-						<h2>@{props.username}</h2>
-						<button 
-							className="reset"
-							onClick={props.onReset.bind(null, props.id)}>
-							Reset
-						</button>
-				</div>
-			</div>
-		)
-}
-
-PlayerPreview.PropTypes = {
-	avatar: PropTypes.string.isRequired,
-	username: PropTypes.string.isRequired,
-	id: PropTypes.string.isRequired,
-	onReset: PropTypes.func.isRequired
-}
+var PlayerPreview = require('./PlayerPreview')
 
 class PlayerInput extends React.Component{
 
@@ -144,8 +120,16 @@ class Battle extends React.Component{
 							<PlayerPreview
 								avatar={playerOneImage}
 								username={playerOneName}
-								onReset={this.handleReset}
-								id='playerOne' />}
+							>
+
+							<button 
+								className="reset"
+								onClick={this.handleReset.bind(null, 'playerOne')}>
+								Reset
+							</button>
+
+							</PlayerPreview>
+							}
 
 						{!playerTwoName && 
 							<PlayerInput 
@@ -157,8 +141,15 @@ class Battle extends React.Component{
 							<PlayerPreview
 								avatar={playerTwoImage}
 								username={playerTwoName}
-								onReset={this.handleReset}
-								id='playerTwo' />}
+							>
+
+							<button 
+								className="reset"
+								onClick={this.handleReset.bind(null, 'playerTwo')}>
+								Reset
+							</button>
+
+							</PlayerPreview>}
 					</div>
 
 					{playerOneImage && playerTwoImage && 
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,17 @@
 var axios = require('axios');
+var id = "YOUR CLIENT ID";
+var sec = "YOUR SECRET";
+var params  = "?client_id=" + id + "&client_secret=" + sec;
+
 
 module.exports = {
+
+	battle: function(players){
+		return axios.all(players.map(getUserData))
+		.then(sortPlayers)
+		.catch(handleError)
+	},
+
 	fetchPopularRepos : function(language){
 		var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
 
@@ -9,4 +20,57 @@ module.exports = {
 				return response.data.items;
 			})
 	}
+}
+
+
+function getProfile(username){
+	return axios.get('https://api.github.com/users/' + username)
+	.then(function(user){
+		return user.data;
+	})
+}
+
+function getRepos(username){
+	return axios.get('https://api.github.com/users/' + username + '/repos' + '?per_page=100')
+}
+
+function getStarCount(repos){
+	console.log('repos', repos)
+	return repos.reduce(function(count, repo){
+		return count + repo.stargazers_count;
+	}, 0)
+}
+
+function calculateScore(profile, repos){
+	/*console.log('profile', profile);
+	console.log('repos', repos.data)*/
+	var followers = profile.followers;
+	var totalStars = getStarCount(repos.data);
+	return (followers * 3) + totalStars
+}
+
+function handleError(error){
+	console.warn(error);
+	return null;
+}
+
+function getUserData(player){
+	return axios.all([
+			getProfile(player),
+			getRepos(player)
+		]).then(function(data){
+			var profile = data[0];
+			var repos = data[1];
+
+			return {
+				profile: profile,
+				score: calculateScore(profile, repos)
+			}
+		})
+}
+
+function sortPlayers(players){
+	return players.sort(function(a, b){
+		return b.score - a.score
+	})
 }
